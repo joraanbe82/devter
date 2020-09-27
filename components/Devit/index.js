fe0407121f347503e63c4bac458c59ba058616e7
Jose Raul Andrade Bernal <joraanbe@gmail.com>
2020-09-27 12:00:25 +0200

    hook para usar timeAgo personalizado

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,6 +1,10 @@
 import Avatar from 'components/Avatar'
+import useTimeAgo from 'hooks/useTimeAgo'
 
 export default function Devit({ avatar, userName, content, createdAt, id }) {
+  const timeago = useTimeAgo(createdAt)
+  // const createdAtFormated = useDateTimeFormat(createdAt)
+
   return (
     <>
       <article>
@@ -11,7 +15,7 @@ export default function Devit({ avatar, userName, content, createdAt, id }) {
           <header>
             <strong>{userName}</strong>
             <span> - </span>
-            <date>{createdAt}</date>
+            <date>{timeago}</date>
           </header>
 
           <p>{content}</p>
